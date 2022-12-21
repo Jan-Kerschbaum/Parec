7b4589bff2b3b974178859f12d524c43abe84ea5
Jan Kerschbaum <jankerschbaum@web.de>
2022-12-21 10:37:35 +0100

    Implemented edge construction

--- parec-backend/app/src/controller.py
+++ parec-backend/app/src/controller.py
@@ -1,17 +1,25 @@
 #File to implement the main controller function for backend operation, to be called from main.py
 from app.src.related_terms import get_term_graph
-from app.src.paper_seach import run_paper_search 
+from app.src.paper_seach import run_paper_search
+import json
 
 #Constants
 SEARCH_DEPTH = 5
 
 #Main control function for backend operation
 #Return values:
-#   edges: List of lists of strings, each element is a list containing the names of two nodes that share an edge
+#   edges: JSON object representing edge list of term graph
 #   papers: List of lists, each element is a list containing the name, authors and id of a specific paper
 def run_backend(query: str):
     # Get term graph for query
     term_graph = get_term_graph(query, SEARCH_DEPTH)
     papers = run_paper_search(term_graph, query)
     # ToDo: Construct edges from term graph
+    running_id = 0
+    edges_dict = {}
+    for key in term_graph.keys():
+        for val in term_graph[key]:
+            edges_dict[running_id] = {"from": key, "to": val}
+            running_id += 1
+    edges = json.dumps(edges_dict)
     return edges, papers
