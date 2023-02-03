4b20cdf07897f4663fec574970e544a01eb52b12
Annalena Frey <pf225@stud.uni-heidelberg.de>
2023-02-03 17:29:37 +0100

    test-skeleton for run_backend function

---
+++ parec-backend/app/tests/test_controller.py
@@ -0,0 +1,10 @@
+import unittest
+from src import controller.py
+
+class TestCases(unitest.TestCase):
+    def test_run_backend(self):
+        self.assertTrue()
+
+
+if __name__ == '__main__':
+    unittest.main()
