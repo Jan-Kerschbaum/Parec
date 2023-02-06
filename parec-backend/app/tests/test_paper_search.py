879149c52c9d6aeca72429b84a32efa1bff8fe5f
Annalena Frey <pf225@stud.uni-heidelberg.de>
2023-02-06 11:54:34 +0100

    added docstrings

---
+++ parec-backend/app/tests/test_paper_search.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestCases(unittest.TestCase):
+    def test_run_paper_search(self):
+
+    def test_construct_relevance_metric(self):
+
+    def test_get_paper_relevance(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ parec-backend/app/tests/test_related_terms.py
@@ -0,0 +1,11 @@
+import unittest
+from scr import *
+
+class TestCases(unittest.TestCase):
+    def test_get_term_graph(self):
+
+    def test_find_related_terms_query(self):
+
+    def test_find_related_terms(self):
+
+    def test_load_model(self):
