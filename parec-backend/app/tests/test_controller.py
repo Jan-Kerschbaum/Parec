20dfdd9d5bac3122ee81e4ab71b294f491ea44bd
Jan Kerschbaum <jankerschbaum@web.de>
2023-02-28 13:26:03 +0100

    Implemented some tests

--- parec-backend/app/tests/test_controller.py
+++ parec-backend/app/tests/test_controller.py
@@ -1,9 +1,20 @@
 import unittest
-from src import controller.py
+from src.controller import run_backend
+import json
 
-class TestCases(unitest.TestCase):
+class TestCases(unittest.TestCase):
     def test_run_backend(self):
-        self.assertTrue()
+        edges_list = []
+        papers_list = []
+        queries = ["ai", "data science", "343dad|?_", "<script>alert('!')</script>"]
+        for i in range(len(queries)):
+            edges_list[i], papers_list[i] = run_backend(queries[i])
+            edges_list[i] = dict(json.loads(edges_list[i]))
+        for i in range(len(queries)):
+            self.assertTrue(len(papers_list[i] > 0))
+            self.assertEqual(type(papers_list[i]), type([]))
+            self.assertTrue(len(edges_list[i].keys()) > 0)
+            self.assertEqual(type(papers_list[i][0]), type([]))
 
 
 if __name__ == '__main__':
--- parec-backend/app/tests/test_related_terms.py
+++ parec-backend/app/tests/test_related_terms.py
@@ -1,11 +1,28 @@
 import unittest
-from scr import *
+from src.related_terms import get_term_graph, find_related_terms, find_related_terms_query
 
 class TestCases(unittest.TestCase):
     def test_get_term_graph(self):
+        query = "query"
+        depth = 2
+        words_per_search = 2
+        graph = get_term_graph(query, depth, words_per_search)
+        self.assertEqual(type(graph), type({}))
+        self.assertEqual(type(list(graph.keys())[0]), type(""))
+        self.assertEqual(type(list(graph.values())[0]), type([]))
+        self.assertEqual(type(list(graph.values())[0][0]), type(""))
 
     def test_find_related_terms_query(self):
+        res = find_related_terms_query("query", 3)
+        self.assertEqual(type(res), type([]))
+        self.assertEqual(type(res), type(""))
 
     def test_find_related_terms(self):
+        query = find_related_terms_query("query", 1)[0] # Making sure that query is an embedded term
+        res = find_related_terms(query, 3)
+        self.assertEqual(type(res), type([]))
+        self.assertEqual(type(res), type(""))
 
-    def test_load_model(self):
+
+if __name__ == '__main__':
+    unittest.main()
